2e4edbbb3f88137a6a29d739143bb6befcb676d9
beliadan <beliadan@fel.cvut.cz>
2024-04-06 13:28:38 +0200

    create project

---
+++ FreelancePlatform/src/main/java/freelanceplatform/FreelancePlatformApplication.java
@@ -0,0 +1,14 @@
+package freelanceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelancePlatformApplication.class, args);
+
+    }
+
+}
---
+++ FreelancePlatform/src/test/java/freelanceplatform/FreelancePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package freelanceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
