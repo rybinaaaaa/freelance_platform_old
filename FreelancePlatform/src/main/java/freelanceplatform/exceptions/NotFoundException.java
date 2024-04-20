5bb32b2d4b392a2756ea831fddba93aac08836f0
beliadan <beliadan@fel.cvut.cz>
2024-04-20 20:57:12 +0200

    resolve conflicts in exception

--- FreelancePlatform/src/main/java/freelanceplatform/exceptions/NotFoundException.java
+++ FreelancePlatform/src/main/java/freelanceplatform/exceptions/NotFoundException.java
@@ -1,16 +1,7 @@
 package freelanceplatform.exceptions;
 
-public class NotFoundException extends BaseException {
-
+public class NotFoundException extends BaseException{
     public NotFoundException(String message) {
         super(message);
-    }
-
-    public NotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public static NotFoundException create(String resourceName, Object identifier) {
-        return new NotFoundException(resourceName + " identified by " + identifier + " not found.");
     }
 }
