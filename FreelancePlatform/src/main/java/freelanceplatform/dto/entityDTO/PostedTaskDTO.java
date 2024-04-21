886da953ee70dc7547c1cc83319a7b62c588955e
beliadan <beliadan@fel.cvut.cz>
2024-04-21 15:32:28 +0200

    resolve conflicts

--- FreelancePlatform/src/main/java/freelanceplatform/dto/entityDTO/PostedTaskDTO.java
+++ FreelancePlatform/src/main/java/freelanceplatform/dto/entityDTO/PostedTaskDTO.java
@@ -5,8 +5,9 @@ import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
+
 
 @Data
 @AllArgsConstructor
