2e434d91d5697c6390323a92bc0ce076da758778
StasKovalchuk1 <64376198+StasKovalchuk1@users.noreply.github.com>
2024-05-19 21:46:18 +0200

    modified structure

--- FreelancePlatform/src/main/java/freelanceplatform/data/FeedbackRepository.java
+++ FreelancePlatform/src/main/java/freelanceplatform/data/FeedbackRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 @Repository
 public interface FeedbackRepository extends CrudRepository<Feedback, Integer> {
-
     List<Feedback> findAll();
 
     List<Feedback> findByReceiver(User receiver);
 
     List<Feedback> findBySender(User sender);
+
 }
