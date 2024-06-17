f464c7740b8901a8d53dd9dd747d225bb7ebc80a
StasKovalchuk1 <64376198+StasKovalchuk1@users.noreply.github.com>
2024-06-17 16:49:50 +0200

    interceptor

---
+++ FreelancePlatform/src/main/java/freelanceplatform/config/WebConfig.java
@@ -0,0 +1,21 @@
+package freelanceplatform.config;
+
+import freelanceplatform.logging.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggerInterceptor loggerInterceptor;
+
+    public WebConfig(LoggerInterceptor loggerInterceptor) {
+        this.loggerInterceptor = loggerInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggerInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ FreelancePlatform/src/main/java/freelanceplatform/logging/LoggerInterceptor.java
@@ -0,0 +1,60 @@
+package freelanceplatform.logging;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Enumeration;
+
+@Component
+public class LoggerInterceptor implements HandlerInterceptor {
+    private static Logger log = LoggerFactory.getLogger(LoggerInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        log.info("[preHandle][" + request + "]" + "[" + request.getMethod() + "]"
+                + "[" + request.getRequestURI() + "]" + "[" + getParameters(request) + "]");
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    private String getParameters(HttpServletRequest request) {
+        StringBuffer posted = new StringBuffer();
+        Enumeration<?> e = request.getParameterNames();
+        if (e != null) {
+            posted.append("?");
+        }
+        while (e.hasMoreElements()) {
+            if (posted.length() > 1) {
+                posted.append("&");
+            }
+            String curr = (String) e.nextElement();
+            posted.append(curr + "=");
+            if (curr.contains("password")
+                    || curr.contains("pass")
+                    || curr.contains("pwd")) {
+                posted.append("*****");
+            } else {
+                posted.append(request.getParameter(curr));
+            }
+        }
+        String ip = request.getHeader("X-FORWARDED-FOR");
+        String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
+        if (ipAddr!=null && !ipAddr.equals("")) {
+            posted.append("&_psip=" + ipAddr);
+        }
+        return posted.toString();
+    }
+
+    private String getRemoteAddr(HttpServletRequest request) {
+        String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
+        if (ipFromHeader != null && ipFromHeader.length() > 0) {
+            log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
+            return ipFromHeader;
+        }
+        return request.getRemoteAddr();
+    }
+}
